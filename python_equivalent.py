e4885fd9126890be491cc584545a94e7fe0f1bb1
Benjamin M. Singleton <bsinglet@vt.edu>
2022-05-24 02:12:18 -0400

    Functional prototype of the python equivalent, although it may need more optimization to be a fair comparison.

--- python_equivalent.py
+++ python_equivalent.py
@@ -1,7 +1,10 @@
 __author__ = 'Benjamin M. Singleton'
 __date__   = '24 May 2022'
 __version  = '0.1.0'
+
 from tqdm import tqdm
+from datetime import datetime
+import ipaddress
 
 desired_ips_filename = 'desired_ips.csv'
 total_ips_filename = 'total_ips.csv'
@@ -10,7 +13,7 @@ total_ips_filename = 'total_ips.csv'
 def get_lines(filename: str) -> list:
     with open(filename, 'r') as input_file:
         lines = input_file.read()
-    lines = lines.trim().split('\n')
+    lines = lines.strip().split('\n')
     return lines
 
 
@@ -19,18 +22,25 @@ def parse_ip_entries(ip_entries: list) -> list:
     for each_entry in tqdm(ip_entries):
         if '-' in each_entry:
             # dashed range
-            continue
+            lower_bound, upper_bound = [int(ipaddress.IPv4Address(x)) for x in each_entry.split('-')]
+            parsed_ips.extend(list(range(lower_bound, upper_bound+1)))
+            #while lower_bound <= upper_bound:
+            #    parsed_ips.append(int(ipaddress.IPv4Address(lower_bound)))
+            #    lower_bound += 1
         elif '/' in each_entry:
             # CIDR block
-            continue
+            network = ipaddress.IPv4Network(each_entry)
+            lower_bound = int(ipaddress.IPv4Address(network.network_address))
+            upper_bound = int(ipaddress.IPv4Address(network.broadcast_address))
+            parsed_ips.extend(list(range(lower_bound, upper_bound+1)))
         else:
             # individual IP address
-            continue
+            parsed_ips.append(int(ipaddress.IPv4Address(each_entry)))
     return parsed_ips
 
 
 def print_with_time(message: str) -> None:
-    pass
+    print(f"{datetime.now().hour:02}:{datetime.now().minute:02}:{datetime.now().second:02}: " + message)
 
 
 def a_in_b(a: set, b: set) -> bool:
@@ -39,21 +49,27 @@ def a_in_b(a: set, b: set) -> bool:
 
 def main():
     # import the entries
+    print_with_time(f"Loading IP range A from {desired_ips_filename}")
     desired_ips = get_lines(desired_ips_filename)
-    total_ips = get_lines(desired_ips_filename)
+    print_with_time(f"Loading IP range B from {total_ips_filename}")
+    total_ips = get_lines(total_ips_filename)
 
     # expand both lists into individual IP addresses
+    print_with_time("Parsing the desired IPs.")
     desired_ips = parse_ip_entries(desired_ips)
+    print_with_time("Parsing the total IPs.")
     total_ips = parse_ip_entries(total_ips)
 
     # convert each list to a set
+    print_with_time("Converting list A to a set.")
     desired_ips = set(desired_ips)
+    print_with_time("Converting list B to a set.")
     total_ips = set(total_ips)
+    print_with_time("Checking if list A is in list B.")
     if a_in_b(desired_ips, total_ips):
-        print("It's a match!")
+        print_with_time("It's a match!")
     else:
-        print("No luck.")
-
+        print_with_time("No luck.")
 
 
 if __name__ == '__main__':
